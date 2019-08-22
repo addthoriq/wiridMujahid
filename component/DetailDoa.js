3b67d862a1bdfbf39c0d40113978b652b8c5b622
addthoriq <alvarro.thoriq@gmail.com>
2019-08-22 21:07:59 +0700

    fix flatlist

--- component/DetailDoa.js
+++ component/DetailDoa.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+    Dimensions,
     FlatList,
     Clipboard,
     TouchableOpacity,
@@ -47,51 +48,63 @@ export default class DetailDoa extends Component
         const {navigation}  = this.props;
         const index         = parseInt(this.id) - 1;
       return (
-          <View style={{flex: 1, backgroundColor: '#e5e5e5'}}>
-             <Header style={{backgroundColor: '#4caf50'}} androidStatusBarColor="green">
-                 <Left>
-                     <Button transparent onPress={()=>this.props.navigation.goBack()}>
-                         <Icon name="arrow-back" />
-                     </Button>
-                 </Left>
-                 <Body>
-                     <Title>Doa ke {DataDoa[index].id}</Title>
-                 </Body>
-                 <Right>
-                     <Button transparent>
-                         <Menu
-                             ref={this.setMenuRef}
-                             button=
-                             {
-                                 <TouchableOpacity onPress={this.showMenu}>
-                                     <Icon name="more" style={{fontSize: 24}} />
-                                 </TouchableOpacity>
-                             }
-                         >
-                             <MenuItem onPress={()=>this.copy()} >
-                                 Salin
-                             </MenuItem>
-                         </Menu>
-                     </Button>
-                 </Right>
-             </Header>
-              <ScrollView pagingEnabled={true}>
-                  <Card>
-                      <CardItem header bordered style={{flex: 1,justifyContent: 'center',alignItems: 'center',}}>
-                          <Text style={{textAlign: 'center', color: '#388e3c'}}>{DataDoa[index].judul}</Text>
-                      </CardItem>
-                      <CardItem>
-                          <Body style={{justifyContent: 'center',alignItems: 'center',}}>
-                              <Text style={s.arb}>{DataDoa[index].arab}</Text>
-                              <Text style={s.art}>{DataDoa[index].arti}</Text>
-                          </Body>
-                      </CardItem>
-                      <CardItem footer bordered style={{flex: 1,justifyContent: 'center',alignItems: 'center',}}>
-                          <Text style={{textAlign: 'center', color: '#388e3c'}}>{DataDoa[index].dalil}</Text>
-                      </CardItem>
-                  </Card>
-              </ScrollView>
-          </View>
+          <FlatList
+              data={DataDoa}
+              keyExtractor={(doa, index)=>index.toString()}
+              getItemLayout={(doa, index)=>(
+                  {length: Dimensions.get('window').width, offset: Dimensions.get('window').width * index, index}
+              )}
+              initialScrollIndex={index}
+              horizontal={true}
+              pagingEnabled={true}
+              renderItem={(doa)=>(
+                  <View style={{width: Dimensions.get('window').width, backgroundColor: '#e5e5e5'}}>
+                     <Header style={{backgroundColor: '#4caf50'}} androidStatusBarColor="green">
+                         <Left>
+                             <Button transparent onPress={()=>this.props.navigation.goBack()}>
+                                 <Icon name="arrow-back" />
+                             </Button>
+                         </Left>
+                         <Body>
+                             <Title>Doa ke {doa.item.id}</Title>
+                         </Body>
+                         <Right>
+                             <Button transparent>
+                                 <Menu
+                                     ref={this.setMenuRef}
+                                     button=
+                                     {
+                                         <TouchableOpacity onPress={this.showMenu}>
+                                             <Icon name="more" style={{fontSize: 24}} />
+                                         </TouchableOpacity>
+                                     }
+                                 >
+                                     <MenuItem onPress={()=>this.copy()} >
+                                         Salin
+                                     </MenuItem>
+                                 </Menu>
+                             </Button>
+                         </Right>
+                     </Header>
+                      <ScrollView pagingEnabled={true}>
+                          <Card>
+                              <CardItem header bordered style={{flex: 1,justifyContent: 'center',alignItems: 'center',}}>
+                                  <Text style={{textAlign: 'center', color: '#388e3c'}}>{doa.item.judul}</Text>
+                              </CardItem>
+                              <CardItem>
+                                  <Body style={{justifyContent: 'center',alignItems: 'center',}}>
+                                      <Text style={s.arb}>{doa.item.arab}</Text>
+                                      <Text style={s.art}>{doa.item.arti}</Text>
+                                  </Body>
+                              </CardItem>
+                              <CardItem footer bordered style={{flex: 1,justifyContent: 'center',alignItems: 'center',}}>
+                                  <Text style={{textAlign: 'center', color: '#388e3c'}}>{DataDoa[index].dalil}</Text>
+                              </CardItem>
+                          </Card>
+                      </ScrollView>
+                  </View>
+              )}
+          />
       );
     }
 }
